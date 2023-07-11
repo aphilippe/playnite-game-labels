b9b8542017f4fe6935497e4f783aaac09ee71b72
Adrien Philippe <adrien.philippe@ubisoft.com>
2023-07-11 18:15:03 +0200

    Add GameLabel object
    add fake gamelabel to test in gameLabelsContainer

---
+++ GameLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GameLabels
+{
+    internal class GameLabel
+    {
+        public string Text { get; set; }
+        public Brush BackgroundColor { get; set; }
+        public Brush TextColor { get; set; }
+    }
+}
